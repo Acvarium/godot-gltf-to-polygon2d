841d33fb17cca0e65fafbc484fe91192c487c4c1
Vitalii Shmorhun <3dvits@gmail.com>
2025-03-05 02:05:35 +0200

    Correct import of basic shape for the poligon 2d

--- addons/gltf_to_polygon2d/import_plugin.gd
+++ addons/gltf_to_polygon2d/import_plugin.gd
@@ -43,7 +43,7 @@ func _import(source_file: String, save_path: String, options: Dictionary, r_plat
 		return ERR_CANT_CREATE
 
 	var polygon2d = Polygon2D.new()
-	
+	polygon2d.name = "Polygon2D"
 	var vertices = []
 
 	_process_node(scene_root, vertices)
@@ -51,8 +51,10 @@ func _import(source_file: String, save_path: String, options: Dictionary, r_plat
 	polygon2d.polygon = vertices
 
 	var node2d = Node2D.new()
+	node2d.name = source_file.get_file().split('.')[0]
 	node2d.add_child(polygon2d, true)
 	polygon2d.owner = node2d
+	
 
 	var packed_scene = PackedScene.new()
 	packed_scene.pack(node2d)
@@ -63,14 +65,73 @@ func _process_node(node: Node, vertices: Array, parent_path: String = ""):
 		var mesh = node.mesh
 		if mesh is ImporterMesh:
 			mesh = mesh.get_mesh()
-		
 		if mesh is ArrayMesh:
-			for i in range(mesh.get_surface_count()):
-				var array = mesh.surface_get_arrays(i)
-				if array.size() > Mesh.ARRAY_VERTEX:
-					var verts = array[Mesh.ARRAY_VERTEX]
-					for v in verts:
-						vertices.append(Vector2(v.x, -v.y))  # Проєкція на XY (вид з Z+)
+			var edges = _extract_boundary_edges(mesh)
+			var ordered_vertices = _order_boundary_vertices(edges)
+			if ordered_vertices.size() > 0:
 
+			
+				var vert_array = []
+				for i in range(mesh.get_surface_count()):
+					var array = mesh.surface_get_arrays(i)
+					if array.size() > Mesh.ARRAY_VERTEX:
+						var verts = array[Mesh.ARRAY_VERTEX]
+						for v in verts:
+							vert_array.append(Vector2(v.x, -v.y))  # Проєкція на XY (вид з Z+)
+							
+				for i in range(ordered_vertices.size()):
+					vertices.append(vert_array[int(ordered_vertices[i])])
 	for child in node.get_children():
 		_process_node(child, vertices, parent_path)
+
+
+func _extract_boundary_edges(mesh: Mesh) -> Dictionary:
+	var edges = {}
+	if mesh is ArrayMesh:
+		for i in range(mesh.get_surface_count()):
+			var array = mesh.surface_get_arrays(i)
+			if array.size() > Mesh.ARRAY_VERTEX:
+				var verts = array[Mesh.ARRAY_VERTEX]
+				var indices = array[Mesh.ARRAY_INDEX] if array.size() > Mesh.ARRAY_INDEX else []
+				for j in range(0, indices.size(), 3):
+					var edge_list = [[indices[j], indices[j+1]], [indices[j+1], indices[j+2]], [indices[j+2], indices[j]]]
+					for edge in edge_list:
+						var key = Vector2(min(edge[0], edge[1]), max(edge[0], edge[1]))
+						print("key " + str(key))
+						if key in edges:
+							edges[key] += 1
+							print("added key")
+						else:
+							edges[key] = 1
+							print("new key")
+	# Заміна dictionary comprehension на цикл
+	var filtered_edges = {}
+	for key in edges.keys():
+		if edges[key] == 1:
+			filtered_edges[key] = edges[key]
+	return filtered_edges
+
+
+func _order_boundary_vertices(edges: Dictionary) -> Array:
+	var edge_keys = edges.keys()
+	if edge_keys.size() == 0:
+		return []
+
+	var ordered = [edge_keys[0].x, edge_keys[0].y]
+	print("edges " + str(edges))
+	while true:
+		var found = false
+		for i in range(edge_keys.size()):
+			if edge_keys[i].x == ordered[-1] and not edge_keys[i].y in ordered:
+				ordered.append(edge_keys[i].y)
+				found = true
+				break
+			elif edge_keys[i].y == ordered[-1] and not edge_keys[i].x in ordered:
+				ordered.append(edge_keys[i].x)
+				found = true
+				break
+		if not found:
+			break
+	for o in ordered:
+		print("o " + str(o))
+	return ordered
